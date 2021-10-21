4cbeba5c3aa4a63f31c0a87d3c41e8a03e1e72af
Eduardo Souza <edu_costaaraujo@hotmail.com>
2021-10-21 18:29:24 -0300

    First commit

---
+++ src/components/MessageList/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { Message } from '../Message';
+import { styles } from './styles';
+"https://github.com/eduardoscosta.png"
+export function MessageList() {
+  const message = {
+    id: "1",
+    text: "minha mensagem",
+    user: {
+      name: "Eduardo",
+      avatar_url: "https://github.com/eduardoscosta.png"
+    }
+  }
+
+  return (
+    <ScrollView style={styles.container}
+      contentContainerStyle={styles.content}
+      keyboardShouldPersistTaps="never">
+      <Message data={message}/>
+      <Message data={message}/>
+      <Message data={message}/>
+    </ScrollView>
+  );
+}
