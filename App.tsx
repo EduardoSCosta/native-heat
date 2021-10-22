3db6abd48f429d17d0269482a4acebe476427d25
Eduardo Souza <edu_costaaraujo@hotmail.com>
2021-10-22 20:07:17 -0300

    Authentication created

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 
 import { Home } from './src/screens/Home';
+import { AuthProvider } from './src/hooks/auth';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,14 +14,16 @@ export default function App() {
 
   if(!fontsLoaded) {
     return (
-      <>
-        <StatusBar style="light"/>
-        <AppLoading />
-      </>
+      <AppLoading />
     );
   }
 
   return (
-    <Home />
+      <AuthProvider>
+        <StatusBar style="light"
+                  translucent
+                  backgroundColor=""/>
+        <Home />
+      </AuthProvider>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, KeyboardAvoidingView, Platform } from 'react-native';
+
 import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
-import { SendMessageForm } from '../../components/SendMessageForm';
 import { SignInBox } from '../../components/SignInBox';
+import { SendMessageForm } from '../../components/SendMessageForm';
+
+import { useAuth } from '../../hooks/auth';
 
 import { styles } from './styles';
 
 export function Home() {
-  return(
-    <View style={styles.container}>
-      <Header />
-      <MessageList />
-      <SendMessageForm />
-    </View>
-  );
+  const { user } = useAuth();
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+    >
+      <View style={styles.container}>
+        <Header />
+        <MessageList />
+
+        {user ? <SendMessageForm /> : <SignInBox />}
+      </View>
+    </KeyboardAvoidingView>
+  )
 }
